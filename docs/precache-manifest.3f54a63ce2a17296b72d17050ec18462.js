241049b9f69ea0039f49a761f3e7a345d1150e92
inmasalcedo <inma.salcedo.gonzalez@gmail.com>
2019-01-07 18:04:53 +0100

    Add docs folder

---
+++ docs/precache-manifest.3f54a63ce2a17296b72d17050ec18462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f28659d67dd1bd0e9f0814c77f8edd88",
+    "url": "/static/media/profile.f28659d6.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "10556734d2f774e4c4c5",
+    "url": "/static/js/main.10556734.chunk.js"
+  },
+  {
+    "revision": "28bb41da4a2a0d025acd",
+    "url": "/static/js/1.28bb41da.chunk.js"
+  },
+  {
+    "revision": "10556734d2f774e4c4c5",
+    "url": "/static/css/main.a53ff6e9.chunk.css"
+  },
+  {
+    "revision": "588eef720d21dbef21a048770bfb6439",
+    "url": "/index.html"
+  }
+];
