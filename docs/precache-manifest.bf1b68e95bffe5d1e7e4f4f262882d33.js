e8c156e97c6af2e706ae9c5d9d7971cfb2e1f891
inmasalcedo <inma.salcedo.gonzalez@gmail.com>
2019-01-22 19:06:19 +0100

    add docs

---
+++ docs/precache-manifest.bf1b68e95bffe5d1e7e4f4f262882d33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f28659d67dd1bd0e9f0814c77f8edd88",
+    "url": "./static/media/profile.f28659d6.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "eca25e1311d215a948a0",
+    "url": "./static/js/main.eca25e13.chunk.js"
+  },
+  {
+    "revision": "c6ac3dd24ab366b4955d",
+    "url": "./static/js/1.c6ac3dd2.chunk.js"
+  },
+  {
+    "revision": "eca25e1311d215a948a0",
+    "url": "./static/css/main.4ab5bb38.chunk.css"
+  },
+  {
+    "revision": "bd1b12005f13ab9696ed8ea9345919d9",
+    "url": "./index.html"
+  }
+];
